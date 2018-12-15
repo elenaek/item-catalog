780fe89150621a1c6f05536337f6cbe677c1874c
Keane Le <keanele12345@gmail.com>
2018-12-15 00:10:14 -0800

    feat: Add update item function and form
    - Add route handling for updating existing item
    - Add template form for updating item
    - Add link to template form in each item page

--- add_categories.py
+++ add_categories.py
@@ -3,28 +3,39 @@ from sqlalchemy.orm import sessionmaker
 from models import Base, Category, Item
 engine = create_engine('sqlite:///itemCatalog.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
+
+def createDBSession():
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
 
 def createCategory(name):
+    session = createDBSession()
     newCategory = Category(name=name)
-    try:
-        session.add(newCategory)
-        session.commit()
-        print("Created {categoryName}".format(categoryName=name))
-    except Exception as e:
-        print("Unable to add {categoryName}".format(categoryName=name))
-        print(e)
+    if session.query(Category).filter_by(name=name).first() is not None:
+        return "This category already exists!"
+    else:
+        try:
+            session.add(newCategory)
+            session.commit()
+            print("Created {categoryName}".format(categoryName=name))
+        except Exception as e:
+            print("Unable to add {categoryName}".format(categoryName=name))
+            print(e)
 
 # Add a bunch of categories into DB
 
 def addItem(name, category_id, description):
-    newItem = Item(name=name, category_id=category_id, description=description)
-    session.add(newItem)
-    session.commit()
-    category = session.query(Category).filter_by(id=category_id).first()
-    print("Created {itemName} under the {categoryName} category!".format(itemName=name, categoryName=category.name))
+    session = createDBSession()
+    if session.query(Item).filter_by(name=name).first() is not None:
+        return "This item already exists!"
+    else:
+        newItem = Item(name=name, category_id=category_id, description=description)
+        session.add(newItem)
+        session.commit()
+        category = session.query(Category).filter_by(id=category_id).first()
+        print("Created {itemName} under the {categoryName} category!".format(itemName=name, categoryName=category.name))
 
 createCategory("Winter Tires")
 createCategory("All Season Tires")
--- app.py
+++ app.py
@@ -29,12 +29,36 @@ def showCategory(category_id):
     items = session.query(Item).filter_by(category_id=category_id).all()
     return render_template('category_items.html', items=items)
 
-# Show Item Page
-@app.route('/catalog/<int:category_id>/<int:item_id>')
-def showItem(category_id, item_id):
+# Show and Create Item Page
+@app.route('/catalog/<int:category_id>/<int:item_id>', methods= ["GET","POST"])
+def interactItem(category_id, item_id):
+    session = createDbSession()
+    if request.method == "GET":
+        item = session.query(Item).filter_by(id=item_id).first()
+        return render_template('item_page.html', item=item)
+    elif request.method == "POST":
+        try:
+            itemToUpdate = session.query(Item).filter_by(id=item_id).first()
+            itemToUpdate.name = request.form.get("name")
+            itemToUpdate.description = request.form.get("description")
+            itemToUpdate.category_id = request.form.get("category")
+            if itemToUpdate.name is not None and itemToUpdate.description is not None and itemToUpdate.category_id is not None:
+                session.add(itemToUpdate)
+                session.commit()
+                return redirect(url_for("interactItem",category_id=itemToUpdate.category_id, item_id=item_id))
+            else:
+                return redirect("/")
+        except Exception as e:
+            print("Error {error}".format(error=e))
+            return url_for("interactItem", category_id=category_id, item_id= item_id)
+
+# Show Item Edit Page
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit')
+def editItem(category_id, item_id):
     session = createDbSession()
     item = session.query(Item).filter_by(id=item_id).first()
-    return render_template('item_page.html', item=item)
+    categories = session.query(Category).all()
+    return render_template('item_edit_page.html', item=item, categories=categories)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
